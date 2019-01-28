bb0700db8c34fabcb8f6bf322749306a9c4879f0
eduardo ubuntu <hceduardo@users.noreply.github.com>
2019-01-28 18:40:10 -0400

    use configuration with memory profiler

--- graph_memory_profiler.py
+++ graph_memory_profiler.py
@@ -1,34 +1,36 @@
 """Provides means to measure graph size in memory in terms of number of edges"""
 
-from inout.parser import parse
+from inout.parser import parse, get_headers
 import config as cfg
 import os
 from memory_profiler import memory_usage
 from time import time
+import sys
 
 __author__ = "Eduardo Hernandez"
 __email__ = "https://www.linkedin.com/in/eduardohernandezj/"
 
+DEFAULT_EDGES_FILE = os.path.join(cfg.BASE_DIR, "sample_files", "girvan_graph.csv")
+
 
 def main():
-    edges_file = 'edges.csv'
-    source_header, target_header, weight_header = 'V1', 'V2', 'Weight'
-    edges_path = os.path.join(cfg.IN_DIR, edges_file)
 
-    edge_limit = 100000
+    edges_path = DEFAULT_EDGES_FILE if len(sys.argv) < 2 else sys.argv[1]
+
+    source_header, target_header, weight_header = get_headers(edges_path)
 
     before_memory = memory_usage()[0]
     before_time = time()
 
     graph = parse(edges_path,
                   source_header=source_header, target_header=target_header, weight_header=weight_header,
-                  edge_limit=edge_limit)
+                  edge_limit=cfg.EDGES_LIMIT)
 
     elapsed_time = time() - before_time
     consumed_memory = memory_usage()[0] - before_memory
 
     print('Number of edges: {0:,}, Graph Size in Memory (MB): {1:5.2f}, Parsing Time (seconds): {2:10.2}'.format(
-        edge_limit, consumed_memory, elapsed_time
+        cfg.EDGES_LIMIT, consumed_memory, elapsed_time
     ))
 
     del graph
